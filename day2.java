2de051b4d8d6fca9864cf565d594188b89ea6915
Jhems <88676905+JdotDEV@users.noreply.github.com>
2023-07-12 16:39:57 +0000

    day2

---
+++ day2.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class SumCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int sum = 0;
+
+        System.out.println("Enter five integers:");
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.print("Enter integer " + i + ": ");
+            int number = scanner.nextInt();
+            sum += number;
+        }
+
+        System.out.println("Sum of the five integers: " + sum);
+
+        scanner.close();
+    }
+}
